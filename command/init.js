92439c7ab77069c7d5938e080bd25e654c866a85
xingyibiaoOffice <1163736962@qq.com>
2017-11-29 17:04:34 +0800

    'webpack地址切换回自己'

--- command/init.js
+++ command/init.js
@@ -2,12 +2,13 @@
  * @Author: xingyibiao 
  * @Date: 2017-11-20 10:09:02 
  * @Last Modified by: xingyibiao
- * @Last Modified time: 2017-11-20 13:35:02
+ * @Last Modified time: 2017-11-29 17:03:28
  */
 'use strict'
 
 const download = require('download-git-repo')
-const WEBPACKURL = 'xingyibiao/webpack-multiple-page'
+// const WEBPACKURL = 'xingyibiao/webpack-multiple-page'
+const WEBPACKURL = 'xingyibiao/webpack'
 
 module.exports = (templateName, dirName) => {
   switch (templateName) {
